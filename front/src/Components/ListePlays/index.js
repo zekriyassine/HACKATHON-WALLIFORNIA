c272df22e5e703484676d0f28f38f7364341811b
massimo <massimo.olivo@gmail.com>
2019-07-03 00:16:30 +0200

    fin css

--- front/src/Components/ListePlays/index.js
+++ front/src/Components/ListePlays/index.js
@@ -21,20 +21,20 @@ class ListePlays extends React.Component {
         return (
             <div>
                 <div className={"playlist " + (this.state.leftPanel ? "active" : "")}>
-                    <button onClick={() => this.listChange('leftPanel')} className="open-button">open</button>
+                    <button onClick={() => this.listChange('leftPanel')} className="open-button">Playlist</button>
                     <ul className="list">
 
                     </ul>
                 </div>
-                <div className="">
-                    <audio className="plays" controls>
+                <div className="plays">
+                    <audio className="" controls>
                         <source src="horse.ogg" type="audio/ogg" />
                         <source src="horse.mp3" type="audio/mpeg" />
                         Your browser does not support the audio element.
                     </audio>
                 </div>
                 <div className={"playlist-left " + (this.state.rightPanel ? "active" : "")}>
-                    <button onClick={() => this.listChange('rightPanel')} className="open-button-left">open</button>
+                    <button onClick={() => this.listChange('rightPanel')} className="open-button-left">Matches</button>
                     <ul className="list_left">
                         <li className="list-item-left">song 1</li>
                         <li className="list-item-left">song 2</li>
