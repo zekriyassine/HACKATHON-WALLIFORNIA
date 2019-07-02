727c85ca1c12996bdf73b6bcd4d73afdf591ab11
Alessandro Cavaleri <caval1289@gmail.com>
2019-07-02 19:10:46 +0200

    token

---
+++ back/config_video/token_key.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const AccessToken = require('twilio').jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
+
+// Used when generating any kind of tokens
+const twilioAccountSid = process.env.TWILIO_ACCOUNTSID;
+const twilioApiKey = process.env.TWILIO_APIKEY;
+const twilioApiSecret = process.env.TWILIO_APISECRET;
+
+const identity = 'user';
+
+// Create Video Grant
+const videoGrant = new VideoGrant({
+  room: 'cool room',
+});
+
+// Create an access token which we will sign and return to the client,
+// containing the grant we just created
+const token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret);
+token.addGrant(videoGrant);
+token.identity = identity;
+
+// Serialize the token to a JWT string
+console.log(token.toJwt());
