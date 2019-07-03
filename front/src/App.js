d47a9366d959aed70c024a1e3de963268078e61f
Alessandro Cavaleri <caval1289@gmail.com>
2019-07-03 06:35:32 +0200

    ok

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ListePlays from './Components/ListePlays/index'
-import 'App.css';
+import './App.css';
 import VideoScreen from './Components/VideoScreen'
 
 function App(){
