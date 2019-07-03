cc68b84d082bff7b4c97dbc401fcc58b65ffae0b
wilder <zekriyassine10@gmail.com>
2019-07-03 06:12:27 +0200

    mise en place de socoket non fini

--- back/app.js
+++ back/app.js
@@ -1,40 +1,53 @@
 const  express  =  require('express');
 const  bodyParser  =  require('body-parser');
 const io = require('socket.io')();
-const  app  =  express(); 
+const  app  =  express();
 const port = 8000;
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended:  false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname  +  '/public'));
+app.use(cors());
+
 
 app.get("/", (req,res) => {
-    res.send("youhou");
+   res.send("youhou");
+})
+
+app.get('/video',(req,res)=>{
+
+ res.json({
+    token
  })
- 
- 
- //je lance le serveur node
 
 
- io.on('connection', socket => {
-    console.log('User connected')
-    
-    socket.on('disconnect', () => {
-      console.log('user disconnected')
-    })
+});
 
-    socket.on('test', (test) => {
-        console.log('test', test)
-        io.emit("test",test)
-    })
-  })
-app.post('/post-playlist',(err,res) =>{
-    const playlist = req.body
-
-    
+app.post('/post-playlist',(req,res) =>{
+  const playlist = req.body
+  console.log(playlist.id)
+  console.log(playlist.name)
+  if(playlist.id == playlist.name){
+    res.json({match : "matched"})
+  }
 
 })
- 
- io.listen(port);
- console.log('listening on port ', port);;
+
+
+io.on('connection', socket => {
+   console.log('User connected')
+
+   socket.on('test', (test) => {
+       console.log('test', test)
+       io.emit("test",test)
+   })
+ })
+ const server = app.listen( process.env.PORT  ||  4000, function(){
+  console.log('Listening on port '  +  server.address().port);
+});
+
+io.listen(port);
+console.log('listening on port ', port);;
+
 
--- front/src/Components/ListePlays/index.js
+++ front/src/Components/ListePlays/index.js
@@ -1,52 +1,120 @@
 import React from 'react';
+import axios from 'axios';
+
 import './index.css';
+import playlist from '../../playlist.json';
+import io from 'socket.io-client'
+const socket = io(`http://localhost:8000`);
+
+
+
+
 
 class ListePlays extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            leftPanel: false,
-            rightPanel: false,
-        }
-
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      playlists: playlist.relationships.tracks.items,
+      leftPanel: false,
+      rightPanel: false,
+      playlist: playlist.relationships.tracks.items,
+      currentSongMp3: "",
+      currentSongName: "",
     }
 
-    listChange = (name) => {
-        this.setState({
-            [name]: !this.state[name]
-        })
-    }
-    render() {
+
+  }
+
+  componentDidMount(prevState) {
+    socket.on("connect", () => {
+      console.log("connecté")
+    })
+  }
+  listChange = (name) => {
+    this.setState({
+      [name]: !this.state[name]
+    })
+  }
+  componentDidUpdate(prevProps,prevState){
+        if(prevState.currentSongName !== this.state.currentSongName){
+          console.log(this.state.currentSongName)
+
+    const url = "http://localhost:4000/post-playlist";
+
+  const config = {
+
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    },
+    body: JSON.stringify({name:this.state.currentSongName,
+                          id : "1"
+    }),
+  };
+  fetch(url, config)
+  .then(res => res.json())
+  .then(data =>(
+    console.log("data")
+  ))
+
+
+  }
+}
+
+  handleClick = (event) => {
+    this.setState({
+      currentSongName: event.target.dataset.name,
+      currentSongMp3: event.target.dataset.url
+
+    })
+
+}
+  render() {
+    const listMusique = this.state.playlists.map(elem => (
+
+      <li onClick={this.handleClick} className="list-item" data-url={elem.data.file} data-name={elem.data.name} >{elem.data.name}</li>
+    ))
+
+    const Player = () => {
+      if (this.state.currentSongMp3) {
         return (
-            <div>
-                <div className={"playlist " + (this.state.leftPanel ? "active" : "")}>
-                    <button onClick={() => this.listChange('leftPanel')} className="open-button">open</button>
-                    <ul className="list">
-
-                    </ul>
-                </div>
-                <div className="">
-                    <audio className="plays" controls>
-                        <source src="horse.ogg" type="audio/ogg" />
-                        <source src="horse.mp3" type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                    </audio>
-                </div>
-                <div className={"playlist-left " + (this.state.rightPanel ? "active" : "")}>
-                    <button onClick={() => this.listChange('rightPanel')} className="open-button-left">open</button>
-                    <ul className="list_left">
-                        <li className="list-item-left">song 1</li>
-                        <li className="list-item-left">song 2</li>
-                        <li className="list-item-left">song 3</li>
-                        <li className="list-item-left">song 4</li>
-                        <li className="list-item-left">song 5</li>
-                    </ul>
-                </div>
-
-            </div>
+          <audio className="plays" controls autoPlay>
+            <source src={this.state.currentSongMp3} type="audio/mp3" />
+          </audio>
         )
+      }
+      return <audio className="plays" controls>
+        <source src="" type="audio/mp3" autoPlay />
+      </audio>
     }
+
+    return (
+      <div>
+        <div className={"playlist " + (this.state.leftPanel ? "active" : "")}>
+          <button onClick={() => this.listChange('leftPanel')} className="open-button">open</button>
+          <ul className="list">
+            {listMusique}
+          </ul>
+        </div>
+        <div className="">
+
+          <Player />
+        </div>
+
+        <div className={"playlist-left " + (this.state.rightPanel ? "active" : "")}>
+          <button onClick={() => this.listChange('rightPanel')} className="open-button-left">open</button>
+          <ul className="list_left">
+            <li className="list-item-left">song 1</li>
+            <li className="list-item-left">song 2</li>
+            <li className="list-item-left">song 3</li>
+            <li className="list-item-left">song 4</li>
+            <li className="list-item-left">song 5</li>
+          </ul>
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default ListePlays;
